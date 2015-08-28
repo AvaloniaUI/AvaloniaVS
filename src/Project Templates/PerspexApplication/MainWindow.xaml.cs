652fe4021934384379c89ae641e9d3c0f95deddd
Steven Kirk <grokys@gmail.com>
2015-08-28 14:47:45 +0200

    Initial commit.

---
+++ src/Project Templates/PerspexApplication/MainWindow.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Perspex.Controls;
+using Perspex.Markup.Xaml;
+
+namespace PerspexApplication
+{
+    public class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            this.InitializeComponent();
+            App.AttachDevTools(this);
+        }
+
+        private void InitializeComponent()
+        {
+            PerspexXamlLoader.Load(this);
+        }
+    }
+}
