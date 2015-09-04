ecd6efe766324ecbb44d23b7d15837cf3001a14a
Darnell Williams <williams.darnell56@yahoo.com>
2015-09-03 21:26:15 -0400

    Add Designer Support Part 1

---
+++ src/PerspexVS/PerspexXamlDocDesigner.cs
@@ -0,0 +1,81 @@
+﻿//------------------------------------------------------------------------------
+// <copyright file="XamlDocListener.cs" company="Company">
+//     Copyright (c) Company.  All rights reserved.
+// </copyright>
+//------------------------------------------------------------------------------
+
+using System;
+using System.Linq;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Forms;
+using System.Windows.Forms.Integration;
+using System.Windows.Media;
+using System.Windows.Threading;
+using Microsoft.VisualStudio.Text;
+using Microsoft.VisualStudio.Text.Editor;
+using Microsoft.VisualStudio.Text.Formatting;
+using Perspex.Designer;
+
+namespace PerspexVS
+{
+    /// <summary>
+    /// XamlDocListener places red boxes behind all the "a"s in the editor window
+    /// </summary>
+    internal sealed class PerspexXamlDocDesigner
+    {
+        private readonly PerspexDesigner Designer;
+        private readonly IWpfTextView view;
+        private readonly ElementHost DesignView;
+
+        public PerspexXamlDocDesigner(IWpfTextView view)
+        {
+            if (view == null)
+            {
+                throw new ArgumentNullException("view");
+            }
+            this.view = view;
+            this.view.TextBuffer.Changed += this.TextBufferOnChanged;
+            DesignView = new ElementHost
+            {
+                Dock = DockStyle.Fill,
+                BackColor = System.Drawing.Color.GreenYellow,
+            };
+            SetupDesignerView(view);
+            DesignView.Child = Designer = new PerspexDesigner
+            {
+                TargetExe = @"C:\Users\Darnell\Documents\GitHub\Perspex\samples\XamlTestApplication\bin\Debug\XamlTestApplication.exe",
+                Xaml = this.view.TextBuffer.CurrentSnapshot.GetText()
+            };
+        }
+
+        private void SetupDesignerView(IWpfTextView view)
+        {
+            var WpfControl = (System.Windows.Controls.Control)view;
+            var timer = new DispatcherTimer() { Interval = TimeSpan.FromMilliseconds(50), IsEnabled = true };
+            timer.Tick += delegate
+            {
+                var wpfWindow = ((System.Windows.Interop.HwndSource)PresentationSource.FromVisual(WpfControl)).Handle;
+                var control = System.Windows.Forms.Control.FromChildHandle(wpfWindow);
+                var otherPanel = control?.Parent.Controls.OfType<System.Windows.Forms.Panel>()
+                    .FirstOrDefault(p => p != control);
+
+                if (otherPanel == null) return;
+                otherPanel.Controls.Add(DesignView);
+                timer.IsEnabled = false;
+            };
+
+            DesignView.Visible =
+                this.view.TextBuffer.CurrentSnapshot.GetText()
+                    .ToLower()
+                    .Contains("=\"https://github.com/grokys/perspex\"");
+        }
+
+        private void TextBufferOnChanged(object sender, TextContentChangedEventArgs e)
+        {
+            var text = e.After.GetText();
+            DesignView.Visible = text.ToLower().Contains("=\"https://github.com/grokys/perspex\"");
+            Designer.Xaml = text;
+        }
+    }
+}
---
+++ src/PerspexVS/XamlDocListener.cs
@@ -0,0 +1,32 @@
+﻿//------------------------------------------------------------------------------
+// <copyright file="XamlDocListenerTextViewCreationListener.cs" company="Company">
+//     Copyright (c) Company.  All rights reserved.
+// </copyright>
+//------------------------------------------------------------------------------
+
+using System.ComponentModel.Composition;
+using System.IO;
+using Microsoft.VisualStudio.Text;
+using Microsoft.VisualStudio.Text.Editor;
+using Microsoft.VisualStudio.Utilities;
+
+namespace PerspexVS
+{
+    [Export(typeof(IWpfTextViewCreationListener))]
+    [ContentType("text")]
+    [TextViewRole(PredefinedTextViewRoles.Document)]
+    internal sealed class XamlDocListener : IWpfTextViewCreationListener
+    {
+
+        public void TextViewCreated(IWpfTextView textView)
+        {
+            ITextDocument document;
+            var rc = textView.TextBuffer.Properties.TryGetProperty<ITextDocument>(typeof(ITextDocument), out document);
+            if (rc != true) return;
+            if (Path.GetFileName(document.FilePath.ToLower()).EndsWith(".xaml"))
+            {
+                new PerspexXamlDocDesigner(textView);
+            }
+        }
+    }
+}
