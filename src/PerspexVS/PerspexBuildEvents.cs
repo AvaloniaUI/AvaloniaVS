0d0e70312bb2829b08852276b0a9661d92ef7f18
Darnell Williams <williams.darnell56@yahoo.com>
2015-09-06 02:59:50 -0400

    Add Designer Support Part 2
    * Add build start/stop detection
    * Removed unused references
    * adjusted injection code to keep trying if not able to find the needed
    entry point (rare)
    * Now displays designer properly
    * Now can detect output path (Improvable, current will not detect change
    without reopening designer)
    * Now detects when designer is closed
    * better namespace detection with fallback

---
+++ src/PerspexVS/PerspexBuildEvents.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using EnvDTE;
+using Microsoft.VisualStudio.Shell;
+
+namespace PerspexVS
+{
+    internal sealed class PerspexBuildEvents
+    {
+        /// <summary>
+        /// Raised when a build operation is started
+        /// </summary>
+        public event Action BuildBegin;
+        private void OnBuildBegin() { BuildBegin.Raise(); }
+
+        /// <summary>
+        /// Reised when a build operation has ended
+        /// </summary>
+        public event Action BuildEnd;
+        private void OnBuildEnd() { BuildEnd.Raise(); }
+         
+
+        public PerspexBuildEvents()
+        {
+            var dte = (DTE) Package.GetGlobalService(typeof (DTE));
+            dte.Events.BuildEvents.OnBuildBegin += PdbeBuildBegin;
+            dte.Events.BuildEvents.OnBuildDone += NotifyBuildEnd;
+        }
+
+        /// <summary>
+        /// Global Build Done Callback
+        /// </summary>
+        /// <param name="scope"></param>
+        /// <param name="action"></param>
+        private void NotifyBuildEnd(vsBuildScope scope, vsBuildAction action)
+        {
+            OnBuildEnd();
+        }
+
+        /// <summary>
+        /// Global Build Start Callback
+        /// </summary>
+        /// <param name="scope"></param>
+        /// <param name="action"></param>
+        private void PdbeBuildBegin(vsBuildScope scope, vsBuildAction action)
+        {
+            OnBuildBegin();
+        }
+    }
+
+    public static class EventHandlerExtensions
+    {
+        /// <summary>
+        /// Raises event with thread and null-ref safety.
+        /// </summary>
+        public static void Raise(this Action handler)
+        {
+            handler?.Invoke();
+        }
+    }
+}
--- src/PerspexVS/XamlDocListener.cs
+++ src/PerspexVS/XamlDocListener.cs
@@ -1,9 +1,4 @@
-﻿//------------------------------------------------------------------------------
-// <copyright file="XamlDocListenerTextViewCreationListener.cs" company="Company">
-//     Copyright (c) Company.  All rights reserved.
-// </copyright>
-//------------------------------------------------------------------------------
-
+﻿using System.Collections.Generic;
 using System.ComponentModel.Composition;
 using System.IO;
 using Microsoft.VisualStudio.Text;
@@ -13,23 +8,45 @@ using Microsoft.VisualStudio.Utilities;
 namespace PerspexVS
 {
     /// <summary>
-    /// Listens to the creation of all textview, but only 
+    /// Listens to the creation of all textview, but only applies the Perspex designer to xaml views
     /// </summary>
     [Export(typeof(IWpfTextViewCreationListener))]
     [ContentType("text")]
     [TextViewRole(PredefinedTextViewRoles.Document)]
     internal sealed class XamlDocListener : IWpfTextViewCreationListener
     {
+        public static XamlDocListener Instance { get; private set; }
 
+        public List<PerspexXamlDocDesigner> PerspexXamlDocDesigners { get; }
+
+        public static PerspexBuildEvents Events { get; private set; }
+
+        /// <summary>
+        /// Creates the static instance, create designer list
+        /// (may need later), finally sets up build events each
+        /// <see cref="PerspexXamlDocDesigner"/> will subscribe to.
+        /// </summary>
+        public XamlDocListener()
+        {
+            Instance = this;
+            PerspexXamlDocDesigners = new List<PerspexXamlDocDesigner>();
+            Events = new PerspexBuildEvents();
+        }
+
+        /// <summary>
+        /// Detects if the IWpfTextView's Document Extensions is .xaml and creates 
+        /// a perspex designer for it if true.
+        /// </summary>
+        /// <param name="textView"></param>
         public void TextViewCreated(IWpfTextView textView)
         {
             ITextDocument document;
-            var rc = textView.TextBuffer.Properties.TryGetProperty<ITextDocument>(typeof(ITextDocument), out document);
+            var rc = textView.TextBuffer.Properties.TryGetProperty(typeof(ITextDocument), out document);
             if (rc != true) return;
-            if (Path.GetFileName(document.FilePath.ToLower()).EndsWith(".xaml"))
-            {
-                new PerspexXamlDocDesigner(textView);
-            }
+            var path = document.FilePath;
+            if (!Path.GetFileName(path.ToLower()).EndsWith(".xaml")) return;
+            var designer = new PerspexXamlDocDesigner(textView, path);
+            PerspexXamlDocDesigners.Add(designer);
         }
     }
 }
