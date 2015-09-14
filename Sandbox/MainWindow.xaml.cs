61acba4393b1ae2ebf849304ccce6035c2b14711
Nikita Tsukanov <keks9n@gmail.com>
2015-09-14 03:27:49 +0300

    Added XML parser

---
+++ Sandbox/MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Sandbox
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            Xml.SelectionChanged += Xml_SelectionChanged;
+            Xml.Text = @"<Root><wa";
+
+        }
+
+        private void Xml_SelectionChanged(object sender, RoutedEventArgs _)
+        {
+
+            try
+            {
+                Results.Text = XmlParser.Parse(Xml.Text.Substring(0, Xml.SelectionStart)).ToString();
+            }
+            catch (Exception e)
+            {
+                Results.Text = e.ToString();
+            }
+        }
+    }
+}
