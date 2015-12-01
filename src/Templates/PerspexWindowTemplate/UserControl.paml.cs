aff31e70c8536eeec05c0eb3a0ddfa031ab9fbf9
Nikita Tsukanov <keks9n@gmail.com>
2015-12-02 00:04:09 +0300

    Added user control template

---
+++ src/Templates/PerspexWindowTemplate/UserControl.paml.cs
@@ -0,0 +1,18 @@
+﻿using Perspex.Controls;
+using Perspex.Markup.Xaml;
+
+namespace $rootnamespace$
+{
+    public class $safeitemrootname$ : UserControl
+    {
+        public $safeitemrootname$()
+        {
+            this.InitializeComponent();
+        }
+
+        private void InitializeComponent()
+        {
+            PerspexXamlLoader.Load(this);
+        }
+    }
+}
