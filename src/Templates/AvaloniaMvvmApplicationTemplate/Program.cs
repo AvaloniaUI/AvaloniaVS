7c9171cc0db147c7fe40fc0b96969d8636217c25
Steven Kirk <grokys@gmail.com>
2018-01-15 11:37:54 +0100

    WIP: Adding MVVM template

---
+++ src/Templates/AvaloniaMvvmApplicationTemplate/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Avalonia;
+using Avalonia.Logging.Serilog;
+
+namespace $safeprojectname$
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BuildAvaloniaApp().Start<MainWindow>(() => new MainWindowViewModel());
+        }
+
+        public static AppBuilder BuildAvaloniaApp()
+            => AppBuilder.Configure<App>()
+                .UsePlatformDetect()
+                .UseReactiveUI()
+                .LogToDebug();
+    }
+}
