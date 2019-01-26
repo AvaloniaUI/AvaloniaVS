379a2720ee80529ab84302629e114ccd078031b6
Steven Kirk <grokys@users.noreply.github.com>
2019-01-27 00:35:54 +0100

    Started adding XAML editor infrastructure.

--- AvaloniaVS/AvaloniaPackage.cs
+++ AvaloniaVS/AvaloniaPackage.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Runtime.InteropServices;
 using System.Threading;
+using Microsoft.VisualStudio;
 using Microsoft.VisualStudio.Shell;
+using Microsoft.VisualStudio.Shell.Interop;
 using Task = System.Threading.Tasks.Task;
 
 namespace AvaloniaVS
@@ -9,6 +11,19 @@ namespace AvaloniaVS
     [Guid(PackageGuidString)]
     [InstalledProductRegistration("#110", "#112", "1.0", IconResourceID = 400)]
     [PackageRegistration(UseManagedResourcesOnly = true, AllowsBackgroundLoading = true)]
+    [ProvideAutoLoad(VSConstants.UICONTEXT.SolutionExistsAndFullyLoaded_string, PackageAutoLoadFlags.BackgroundLoad)]
+    [ProvideEditorFactory(typeof(EditorFactory), 113, TrustLevel = __VSEDITORTRUSTLEVEL.ETL_AlwaysTrusted)]
+    [ProvideEditorLogicalView(typeof(EditorFactory), LogicalViewID.Designer)]
+    [ProvideXmlEditorChooserDesignerView("Avalonia",
+        "xaml",
+        LogicalViewID.Designer,
+        10000,
+        Namespace = "https://github.com/avaloniaui",
+        MatchExtensionAndNamespace = true,
+        CodeLogicalViewEditor = typeof(EditorFactory),
+        DesignerLogicalViewEditor = typeof(EditorFactory),
+        DebuggingLogicalViewEditor = typeof(EditorFactory),
+        TextLogicalViewEditor = typeof(EditorFactory))]
     public sealed class AvaloniaPackage : AsyncPackage
     {
         public const string PackageGuidString = "894B5FA9-7669-4E8A-81DE-709F18B47CEE";
@@ -17,7 +32,9 @@ namespace AvaloniaVS
             CancellationToken cancellationToken,
             IProgress<ServiceProgressData> progress)
         {
+            await base.InitializeAsync(cancellationToken, progress);
             await this.JoinableTaskFactory.SwitchToMainThreadAsync(cancellationToken);
+            RegisterEditorFactory(new EditorFactory(this));
         }
     }
 }
---
+++ AvaloniaVS/EditorFactory.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Microsoft.VisualStudio;
+using Microsoft.VisualStudio.Shell;
+using Microsoft.VisualStudio.Shell.Interop;
+using IOleServiceProvider = Microsoft.VisualStudio.OLE.Interop.IServiceProvider;
+
+namespace AvaloniaVS
+{
+    public sealed class EditorFactory : IVsEditorFactory, IDisposable
+    {
+        readonly AvaloniaPackage _package;
+        ServiceProvider _serviceProvider;
+
+        public EditorFactory(AvaloniaPackage package) => _package = package;
+
+        public int Close() => VSConstants.S_OK;
+
+        public int CreateEditorInstance(uint grfCreateDoc, string pszMkDocument, string pszPhysicalView, IVsHierarchy pvHier, uint itemid, IntPtr punkDocDataExisting, out IntPtr ppunkDocView, out IntPtr ppunkDocData, out string pbstrEditorCaption, out Guid pguidCmdUI, out int pgrfCDW)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Dispose()
+        {
+            ThreadHelper.ThrowIfNotOnUIThread();
+            _serviceProvider?.Dispose();
+            _serviceProvider = null;
+        }
+
+        public int SetSite(IOleServiceProvider psp)
+        {
+            _serviceProvider = new ServiceProvider(psp);
+            return VSConstants.S_OK;
+        }
+
+        public int MapLogicalView(ref Guid rguidLogicalView, out string pbstrPhysicalView)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
