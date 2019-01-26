e74f14337daf2331114d1dd6a8917da9d9ff33e5
Steven Kirk <grokys@users.noreply.github.com>
2019-01-27 00:19:55 +0100

    Added package.

---
+++ AvaloniaVS/AvaloniaPackage.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Threading;
+using Microsoft.VisualStudio.Shell;
+using Task = System.Threading.Tasks.Task;
+
+namespace AvaloniaVS
+{
+    [Guid(PackageGuidString)]
+    [InstalledProductRegistration("#110", "#112", "1.0", IconResourceID = 400)]
+    [PackageRegistration(UseManagedResourcesOnly = true, AllowsBackgroundLoading = true)]
+    public sealed class AvaloniaPackage : AsyncPackage
+    {
+        public const string PackageGuidString = "894B5FA9-7669-4E8A-81DE-709F18B47CEE";
+
+        protected override async Task InitializeAsync(
+            CancellationToken cancellationToken,
+            IProgress<ServiceProgressData> progress)
+        {
+            await this.JoinableTaskFactory.SwitchToMainThreadAsync(cancellationToken);
+        }
+    }
+}
