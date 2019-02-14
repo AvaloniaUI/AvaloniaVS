255ec58baf16e1f061e3a581510fd6816ba6a99c
Steven Kirk <grokys@users.noreply.github.com>
2019-02-14 20:49:51 +0100

    Updated templates.
    To use `AvaloniaResource`, `d:DesignWidth`/`d:DesignHeight` and general tidy-up.

--- templates/AvaloniaMvvmApplicationTemplate/ViewModels/MainWindowViewModel.cs
+++ templates/AvaloniaMvvmApplicationTemplate/ViewModels/MainWindowViewModel.cs
@@ -6,6 +6,6 @@ namespace $safeprojectname$.ViewModels
 {
     public class MainWindowViewModel : ViewModelBase
     {
-        public string Greeting => "Hello World!";
+        public string Greeting => "Welcome to Avalonia!";
     }
 }
