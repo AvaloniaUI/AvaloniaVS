8aaa6c35955ad29350ea915f11848f57893c411c
Andrey Kunchev <donandren@gmail.com>
2019-10-19 20:30:57 +0300

    support completion of local resm and avares urls with passing assembly name to completion
    # Conflicts:
    #	AvaloniaVS/Views/AvaloniaDesigner.xaml.cs

--- AvaloniaVS/Views/AvaloniaDesigner.xaml.cs
+++ AvaloniaVS/Views/AvaloniaDesigner.xaml.cs
@@ -355,6 +355,7 @@ namespace AvaloniaVS.Views
                 var metadata = buffer.Properties.GetOrCreateSingletonProperty(
                     typeof(XamlBufferMetadata),
                     () => new XamlBufferMetadata());
+                buffer.Properties["AssemblyName"] = Path.GetFileNameWithoutExtension(assemblyPath);
 
                 if (metadata.CompletionMetadata == null)
                 {
