91669932e2381bb9b81152c8f56fba966697138a
Steven Kirk <grokys@gmail.com>
2019-11-28 09:10:13 +0100

    Add options dialog.
    Co-Authored-By: Andrey Kunchev <donandren@users.noreply.github.com>

---
+++ AvaloniaVS/Views/OptionsDialogPage.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.ComponentModel;
+using System.Runtime.InteropServices;
+using System.Windows;
+using AvaloniaVS.Services;
+using Microsoft.VisualStudio.Shell;
+
+namespace AvaloniaVS.Views
+{
+    [ClassInterface(ClassInterfaceType.AutoDual)]
+    [ComVisible(true)]
+    [Guid("3093ca7c-c764-4547-a7ae-12055b139bdf")]
+    public class OptionsDialogPage : UIElementDialogPage
+    {
+        private OptionsView _options;
+
+        protected override UIElement Child => _options ?? (_options = new OptionsView());
+
+        protected override void OnActivate(CancelEventArgs e)
+        {
+            base.OnActivate(e);
+
+            _options.Settings = Site.GetMefService<IAvaloniaVSSettings>();
+        }
+
+        public override void SaveSettingsToStorage()
+        {
+            base.SaveSettingsToStorage();
+
+            _options?.Settings?.Save();
+        }
+
+        public override void LoadSettingsFromStorage()
+        {
+            base.LoadSettingsFromStorage();
+
+            _options?.Settings?.Load();
+        }
+    }
+}
---
+++ AvaloniaVS/Views/OptionsView.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System.Windows.Controls;
+using AvaloniaVS.Services;
+
+namespace AvaloniaVS.Views
+{
+    public partial class OptionsView : UserControl
+    {
+        public OptionsView()
+        {
+            InitializeComponent();
+        }
+
+        public IAvaloniaVSSettings Settings
+        {
+            get => DataContext as IAvaloniaVSSettings;
+            set => DataContext = value;
+        }
+    }
+}
