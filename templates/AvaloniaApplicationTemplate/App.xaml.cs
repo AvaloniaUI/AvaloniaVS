72d81ffd2f37c16ae08edd1b16ab9987107552f2
Steven Kirk <grokys@gmail.com>
2019-12-02 12:35:15 +0100

    Update templates to 0.9.0-preview9.

--- templates/AvaloniaApplicationTemplate/App.xaml.cs
+++ templates/AvaloniaApplicationTemplate/App.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using Avalonia;
+using Avalonia.Controls.ApplicationLifetimes;
 using Avalonia.Markup.Xaml;
 
 namespace $safeprojectname$
@@ -8,6 +9,16 @@ namespace $safeprojectname$
         public override void Initialize()
         {
             AvaloniaXamlLoader.Load(this);
+        }
+
+        public override void OnFrameworkInitializationCompleted()
+        {
+            if (ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktop)
+            {
+                desktop.MainWindow = new MainWindow();
+            }
+
+            base.OnFrameworkInitializationCompleted();
         }
     }
 }
