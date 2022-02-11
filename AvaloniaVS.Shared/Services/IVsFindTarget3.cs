59e1f8bfb11b93ce00bf069db22b57f738a44315
Olivier DALET <olivier.dalet@addupsolutions.com>
2022-02-11 17:06:05 +0100

    Fix #231
    Fix #231

--- AvaloniaVS.Shared/Services/IVsFindTarget3.cs
+++ AvaloniaVS.Shared/Services/IVsFindTarget3.cs
@@ -1,9 +1,11 @@
 ﻿using System;
 using System.Runtime.InteropServices;
 
-namespace AvaloniaVS.Services
+// Do not change the namespace; otherwise, search box is broken in VS2022
+namespace Microsoft.VisualStudio.Editor.Internal
 {
     [ComImport]
+    [TypeIdentifier]
     [Guid("A2F0D62B-D0DD-4C59-AAB8-79CD20785451")]
     [InterfaceType(ComInterfaceType.InterfaceIsIUnknown)]
     public interface IVsFindTarget3
