74ed39df6067e3ae5788221d17aaaad75962e08f
Tako <tako0qq@gmail.com>
2022-03-21 16:59:34 +0200

    Add ability to zoom in and out with ctrl + mousewheel

--- AvaloniaVS.Shared/Views/AvaloniaPreviewer.xaml.cs
+++ AvaloniaVS.Shared/Views/AvaloniaPreviewer.xaml.cs
@@ -78,6 +78,20 @@ namespace AvaloniaVS.Views
 
                 e.Handled = true;
             }
+            else if (Keyboard.Modifiers == ModifierKeys.Control)
+            {
+                var designer = FindParent<AvaloniaDesigner>(this);
+                var currentZoomLevelIndex = Array.IndexOf(AvaloniaDesigner.ZoomLevels, designer.ZoomLevel);
+                if (e.Delta < 0 && currentZoomLevelIndex >= 0 && currentZoomLevelIndex + 1 < AvaloniaDesigner.ZoomLevels.Length - 1)
+                {
+                    designer.ZoomLevel = AvaloniaDesigner.ZoomLevels[currentZoomLevelIndex + 1];
+                }
+                else if (e.Delta > 0 && currentZoomLevelIndex > 0)
+                {
+                    designer.ZoomLevel = AvaloniaDesigner.ZoomLevels[currentZoomLevelIndex - 1];
+                }
+                e.Handled = true;
+            }
 
             base.OnPreviewMouseWheel(e);
         }
@@ -199,6 +213,22 @@ namespace AvaloniaVS.Views
                 Button = GetButton(e.ChangedButton),
                 Modifiers = GetModifiers(e),
             });
+        }
+        private static T FindParent<T>(DependencyObject child) where T : DependencyObject
+        {
+            //get parent item
+            DependencyObject parentObject = VisualTreeHelper.GetParent(child);
+
+            //we've reached the end of the tree
+            if (parentObject == null)
+                return null;
+
+            //check if the parent matches the type we're looking for
+            T parent = parentObject as T;
+            if (parent != null)
+                return parent;
+            else
+                return FindParent<T>(parentObject);
         }
 
         private static AvMouseButton GetButton(WpfMouseButton button)
