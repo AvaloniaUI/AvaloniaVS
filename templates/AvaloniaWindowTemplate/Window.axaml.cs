4c3d6cf14f48adfd0cf0251b65256613202ccf7c
Max Katz <maxkatz6@outlook.com>
2022-05-31 15:16:03 -0400

    Update main window too

--- templates/AvaloniaWindowTemplate/Window.axaml.cs
+++ templates/AvaloniaWindowTemplate/Window.axaml.cs
@@ -1,6 +1,4 @@
-using Avalonia;
 using Avalonia.Controls;
-using Avalonia.Markup.Xaml;
 
 namespace $rootnamespace$
 {
@@ -9,14 +7,6 @@ namespace $rootnamespace$
         public $safeitemrootname$()
         {
             InitializeComponent();
-#if DEBUG
-            this.AttachDevTools();
-#endif
-        }
-
-        private void InitializeComponent()
-        {
-            AvaloniaXamlLoader.Load(this);
         }
     }
 }
