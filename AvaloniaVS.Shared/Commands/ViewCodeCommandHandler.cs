e681dd7edec1b13026da9e7cb552ee35dd45ac20
romgerman <romgerman@users.noreply.github.com>
2022-07-10 15:37:03 +0300

    feat: Pressing F7 toggles designer/code view

---
+++ AvaloniaVS.Shared/Commands/ViewCodeCommandHandler.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.ComponentModel.Composition;
+
+using EnvDTE;
+
+using Microsoft.VisualStudio.Commanding;
+using Microsoft.VisualStudio.Shell;
+using Microsoft.VisualStudio.Text.Editor.Commanding.Commands;
+using Microsoft.VisualStudio.Utilities;
+
+namespace AvaloniaVS.Shared.Commands
+{
+    [Export(typeof(ICommandHandler))]
+    [ContentType("text")]
+    [Name("ViewCodeCommandHandler")]
+    internal class ViewCodeCommandHandler : ICommandHandler<ViewCodeCommandArgs>
+    {
+        public string DisplayName => "Toogle Code/Designer";
+
+        public bool ExecuteCommand(ViewCodeCommandArgs args, CommandExecutionContext executionContext)
+        {
+            ThreadHelper.ThrowIfNotOnUIThread();
+
+            var dte = (DTE)AvaloniaPackage.GetGlobalService(typeof(DTE));
+
+            if (args.TextView.Properties.ContainsProperty(typeof(AvaloniaVS.Views.AvaloniaDesigner)))
+            {
+                dte.ItemOperations.OpenFile(dte.ActiveDocument.FullName, Constants.vsViewKindTextView);
+            }
+            else
+            {
+                dte.ItemOperations.OpenFile(dte.ActiveDocument.FullName, Constants.vsViewKindDesigner);
+            }
+
+            return true;
+        }
+
+        public CommandState GetCommandState(ViewCodeCommandArgs args)
+        {
+            if (args.SubjectBuffer.Properties.TryGetProperty<Services.PreviewerProcess>(typeof(Services.PreviewerProcess), out var process))
+            {
+                return CommandState.Available;
+            }
+            else
+            {
+                return CommandState.Unavailable;
+            }
+        }
+    }
+}
