f9a906ba8abf45fe5b8ad1c9f5c00305c351e528
Max Katz <maxkatz6@outlook.com>
2023-02-08 02:55:59 -0500

    Fix build

--- tests/CompletionEngineTests/Metadata/MetadataTestClass.cs
+++ tests/CompletionEngineTests/Metadata/MetadataTestClass.cs
@@ -15,7 +15,9 @@ namespace CompletionEngineTests
         /// Field which should be recognized as attached event,
         /// as its declaring type is sublcass of <see cref="RoutedEvent"/>
         /// </summary>
+#pragma warning disable CA2211 // Non-constant fields should not be visible
         public static MetadataTestClass field;
+#pragma warning restore CA2211 // Non-constant fields should not be visible
 
         public MetadataTestClass(string name, RoutingStrategies routingStrategies, Type eventArgsType, Type ownerType) : base(name, routingStrategies, eventArgsType, ownerType)
         {
