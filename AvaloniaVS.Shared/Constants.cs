d962703363a1dacc20e200f83e0ff7b719219dc2
Giuseppe Lippolis <workgroupengineering@outlook.com>
2023-03-16 18:09:22 +0100

    fix: Open Avalonia Xaml Designer on double click on Errors list

---
+++ AvaloniaVS.Shared/Constants.cs
@@ -0,0 +1,13 @@
+﻿namespace AvaloniaVS;
+
+internal static class Constants
+{
+    public const string PackageGuidString = "865ba8d5-1180-4bf8-8821-345f72a4cb79";
+    public const string PackageName = "Avalonia Xaml Editor";
+    public const string axaml = nameof(axaml);
+    public const string xaml = nameof(xaml);
+    public const string paml = nameof(paml);
+
+    public const string AvaloviaFactoryEditorGuidString = @"6D5344A2-2FCD-49DE-A09D-6A14FD1B1224";
+
+}
